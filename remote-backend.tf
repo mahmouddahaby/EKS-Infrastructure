7b92bd18dd55b8ea60c0177ab2ad57feb04fb858
MahmoudDahaby <99130650+mahmouddahaby@users.noreply.github.com>
2023-04-02 21:12:59 +0200

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,12 +1,14 @@
+# Create an S3 bucket to store Terraform state files
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-up-and-running-state-FS"
 
+  # Prevent the bucket from being accidentally deleted
   lifecycle {
     prevent_destroy = false
-
   }
 }
 
+# Enable versioning for the S3 bucket created earlier
 resource "aws_s3_bucket_versioning" "enabled" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
@@ -14,17 +16,19 @@ resource "aws_s3_bucket_versioning" "enabled" {
   }
 }
 
+# Create a DynamoDB table to manage locks for Terraform state
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-up-and-running-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
   attribute {
     name = "LockID"
     type = "S"
   }
 }
 
-
+# Configure the Terraform backend to store state in S3 and use DynamoDB to manage locks
 terraform {
   backend "s3" {
     bucket         = "terraform-up-and-running-state-haidy"
@@ -32,6 +36,5 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
-
   }
 }
